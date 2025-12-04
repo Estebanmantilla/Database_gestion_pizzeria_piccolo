b4355ca070136ce4668a108241c29ed6271e85a7
esteban <estemantillacar@gmail.com>
2025-12-04 07:30:22 -0500

    feat :sparkles: fix consultas5

--- vistas.sql
+++ vistas.sql
@@ -1,6 +1,6 @@
 -- 1 Vista de resumen de pedidos por cliente (nombre del cliente, cantidad de pedidos, total gastado).
 create view vista_resumen_pedidos_cliente as
-select c.id as id_cliente, c.nombre as nombre_cliente, count(p.id) as cantidad_pedidos, sum(d.valor_venta) as total_gastado from cliente c left join pedido p on c.id = p.id_cliente left join domicilio d on p.id = d.id_pedido group by c.id, c.nombre;
+select c.id as id_cliente, pe.nombre as nombre_cliente, count(p.id) as cantidad_pedidos, sum(d.valor_venta) as total_gastado from persona pe left join cliente c on c.id = pe.id left join pedido p on c.id = p.id_cliente left join domicilio d on p.id = d.id_pedido group by c.id, pe.nombre;
 
 -- 2 Vista de desempeño de repartidores (número de entregas, tiempo promedio, zona).
 create view vista_desempeno_repartidores as
