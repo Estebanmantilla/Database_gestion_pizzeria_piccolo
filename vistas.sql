6b3d189b9a4084a2d54d7d277eeb85e1baede30c
esteban <estemantillacar@gmail.com>
2025-12-04 07:36:31 -0500

    feat :sparkles: fix vista

--- vistas.sql
+++ vistas.sql
@@ -4,7 +4,7 @@ select c.id as id_cliente, pe.nombre as nombre_cliente, count(p.id) as cantidad_
 
 -- 2 Vista de desempeño de repartidores (número de entregas, tiempo promedio, zona).
 create view vista_desempeno_repartidores as
-select r.id as id_repartidor, p.nombre as nombre_repartidor, d.zona, count(d.id) as numero_entregas, avg(timestampdiff(minute, d.hora_salida, d.hora_entrega)) as tiempo_promedio_min from repartidor r left join persona p on r.id = p.id left join domicilio d on r.id = d.id_repartidor group by r.id, p.nombre, d.zona;
+select r.id as id_repartidor, p.nombre as nombre_repartidor, r.zona, count(d.id) as numero_entregas, avg(timestampdiff(minute, d.hora_salida, d.hora_entrega)) as tiempo_promedio_min from repartidor r left join persona p on r.id = p.id left join domicilio d on r.id = d.id_repartidor group by r.id, p.nombre, r.zona;
 
 -- 3 Vista de stock de ingredientes por debajo del mínimo permitido.
 create view vista_stock_bajo as
